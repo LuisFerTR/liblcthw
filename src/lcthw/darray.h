8f104e7c51dbe470e4a181ea8251e1db1bd77031
Luis Talavera <luisfertalavera15@gmail.com>
2020-05-27 20:13:51 -0500

    DArray agregado y probado con éxito.

--- src/lcthw/darray.h
+++ src/lcthw/darray.h
@@ -25,11 +25,44 @@ void DArray_clear_destroy(DArray *array);
 #define DArray_first(A) ((A)->contents[0])
 #define DArray_end(A) ((A)->end)
 #define DArray_count(A) DArray_end(A)
-#define DArray_max ((A)->max)
+#define DArray_max(A) ((A)->max)
 
 #define DEFAULT_EXPAND_RATE 300
 
-static inline void DArray_set
+static inline void DArray_set(DArray *array, int i, void *el) {
+    check(i < array->max, "darray attempt to get past max.");
+    if (i > array->end) {
+        array->end = i;
+    }
+    array->contents[i] = el;
+error:
+    return;
+}
 
+static inline void *DArray_get(DArray *array, int i) {
+    check(i < array->max, "darray attempt to get past max.");
+    return array->contents[i];
+error:
+    return NULL;
+}
+
+static inline void *DArray_remove(DArray *array, int i) {
+    void *el = array->contents[i];
+
+    array->contents[i] = NULL;
+
+    return el;
+}
+
+static inline void *DArray_new(DArray *array) {
+    check(array->element_size > 0, "Can't use DArray_new on 0 size darrays.");
+
+    return calloc(1, array->element_size);
+
+error:
+    return NULL;
+}
+
+#define DArray_free(E) free((E))
 
 #endif
